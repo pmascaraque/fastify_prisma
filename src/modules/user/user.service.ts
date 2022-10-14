0c30ada3d3e7ab32f78f87a1e3aa85de3b1d4443
Pablo Moreno <ssm.pablo@gmail.com>
2022-10-14 17:49:22 +0200

    hashing passwords

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,13 @@
+import { hashPassword } from "../../utils/hash";
 import { prisma } from "../../utils/prisma"
 import { CreateUserInput } from "./user.schema"
 
 export const createUser = async (input: CreateUserInput) => {
+  const { password, ...rest } = input;
+
+  const { hash, salt } = hashPassword(password)
+
   const user = await prisma.user.create({
-    data: input, 
+    data: { ...rest, salt, password: hash }
   })
 }
