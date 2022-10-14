d3a03fa82360a88d9a3fcf362be07b34ae17041c
Pablo Moreno <ssm.pablo@gmail.com>
2022-10-14 14:49:20 +0200

    server setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('hello world')
+import Fastify from "fastify";
+
+const server = Fastify()
+
+const main = async () => {
+  try {
+    await server.listen({ port: 3000, host: '0.0.0.0' })
+    console.log('Server ready at http://localhost:3000')
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+main();
